7282bc383d7f9f4e1b031c2dda2934236f92e81f
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-06-29 17:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (79.14%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int x = 0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[x]=nums[i];
+                x++;
+            }
+        }
+
+        while(x<nums.size()){
+            nums[x] = 0;
+            x++;
+        }
+    }
+};
