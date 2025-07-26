2c9fcbd36e2f35409eab0420e3b984154c926f5a
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-07-26 15:10:30 +0530

    Time: 164 ms (66.10%) | Memory: 324.4 MB (28.81%) - LeetSync

---
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
+
+        long long ans = 0;
+        vector<vector<int>> right(n + 1);
+
+        for (auto& pair : conflictingPairs) {
+            right[max(pair[0], pair[1])].push_back(min(pair[0], pair[1]));
+        }
+
+        long long firstMax = 0;
+        long long secondMax = 0;
+        vector<long long> bonus(n + 1, 0);
+
+        for (int r = 1; r <= n; r++) {
+            for (int left : right[r]) {
+
+                if(left > firstMax) {
+                    secondMax = firstMax;
+                    firstMax = left;
+                } else if(left > secondMax) {
+                    secondMax = left;
+                }
+
+            }
+
+            ans += r - firstMax;  
+
+            if (firstMax > 0) {
+                bonus[firstMax] += firstMax - secondMax;
+            }      
+
+        }
+
+        long long max_bonus = 0;
+        for (long long b : bonus) {
+            max_bonus = std::max(max_bonus, b);
+        }
+
+        return ans + max_bonus;
+
+    }
+};
