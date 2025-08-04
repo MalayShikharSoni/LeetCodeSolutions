bd0b9a40869ea6ec848ebcbc537e7a73e60d841a
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-08-04 14:21:45 +0530

    Time: 43 ms (48.66%) | Memory: 75.2 MB (26.07%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        int start = 0;
+        int maxx = 0;
+        unordered_map<int, int> basket;
+
+        for(int end = 0; end < fruits.size(); end++) {
+
+            basket[fruits[end]]++;
+
+            while(basket.size() > 2) {
+                
+                basket[fruits[start]]--;
+
+                if(basket[fruits[start]] == 0) {
+                    basket.erase(fruits[start]);
+                }
+                start++;
+
+            }
+
+            maxx = max(maxx, end - start + 1);
+
+        }
+
+        return maxx;
+
+    }
+};
