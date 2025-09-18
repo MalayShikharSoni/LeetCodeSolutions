0ed1c0e381f2c49f1d21ecd7819e072048ae7297
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-09-18 21:05:46 +0530

    Time: 151 ms (98.75%) | Memory: 342 MB (99.69%) - LeetSync

--- 3678-design-task-manager/design-task-manager.cpp
+++ 3678-design-task-manager/design-task-manager.cpp
@@ -1,43 +1,40 @@
 class TaskManager {
-    using int2=pair<int , int>; 
-    unordered_map<int, int2> mp;// taskID->(prioity, userId)
-    priority_queue<int2> pq; //(priority, taskID)
+    unordered_map<int, pair<int, int>> mp;
+    priority_queue<pair<int, int>> pq;
+
 public:
     TaskManager(vector<vector<int>>& tasks) {
         mp.reserve(tasks.size());
-        for(auto& t: tasks){
-            int  u=t[0], i=t[1], p=t[2];
-            mp[i]={p, u};
+        for (auto& t : tasks) {
+            int u = t[0], i = t[1], p = t[2];
+            mp[i] = {p, u};
             pq.emplace(p, i);
         }
     }
-    
+
     void add(int userId, int taskId, int priority) {
-        mp[taskId]={priority, userId};
+        mp[taskId] = {priority, userId};
         pq.emplace(priority, taskId);
     }
-    
+
     void edit(int taskId, int newPriority) {
-        mp[taskId].first=newPriority;
+        mp[taskId].first = newPriority;
         pq.emplace(newPriority, taskId);
     }
-    
-    void rmv(int taskId) {
-        mp[taskId].first=-1;
-    }
-    
+
+    void rmv(int taskId) { mp[taskId].first = -1; }
+
     int execTop() {
         while (!pq.empty()) {
             auto [p, i] = pq.top();
             if (mp.count(i) && mp[i].first == p) {
                 pq.pop();
-                int u=mp[i].second;
-                mp.erase(i); // Remove task after execution
+                int u = mp[i].second;
+                mp.erase(i);
                 return u;
             }
-            pq.pop(); // Remove
+            pq.pop();
         }
-        return -1; // No valid tasks to execute
-        
+        return -1;
     }
 };
