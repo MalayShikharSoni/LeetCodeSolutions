0250fbb0846b3c8643e7eca6f272bc9337819c42
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-11-21 12:24:40 +0530

    Time: 165 ms (46.52%) | Memory: 15.9 MB (58.20%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int ans=0;
-        unordered_map<char, pair<int,int>> mp;
+        int ans = 0;
+        unordered_map<char, pair<int, int>> mp;
 
-        for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])==mp.end()){
-                mp[s[i]].first=i;
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.find(s[i]) == mp.end()) {
+                mp[s[i]].first = i;
             }
-            mp[s[i]].second=i;
+            mp[s[i]].second = i;
         }
 
-        for(auto& [c,p] : mp){
-            int left=p.first;
-            int right=p.second;
+        for (auto& [c, p] : mp) {
+            int left = p.first;
+            int right = p.second;
 
-            if(right-left<=1) continue;
+            if (right - left <= 1)
+                continue;
 
             unordered_set<char> sett;
-            for(int i=left+1;i<right;i++){
+            for (int i = left + 1; i < right; i++) {
                 sett.insert(s[i]);
             }
 
-            ans+=sett.size();
+            ans += sett.size();
         }
 
         return ans;
