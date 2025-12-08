e0743f6cb76ee9f2da72d6bccfea1fda197e669e
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2025-12-08 20:33:54 +0530

    Time: 134 ms (23.67%) | Memory: 7.8 MB (84.51%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
+    bool perfectSquare(int n) {
+
+        if (n == 1)
+            return true;
+        if (n == 4) 
+            return true;
+        int left = 1;
+
+        int right = n / 2;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+            if (mid * mid == n)
+                return true;
+
+            else if (mid * mid >= n) {
+                right = mid;
+            }
+
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
     int countTriples(int n) {
 
         int count = 0;
-        
+
         for(int a = 1; a <= n - 1; a++) {
-            
+
             for(int b = 1; b <= n - 1; b++) {
 
                 int cSquare = a * a + b * b;
-                int cSecond = (int)sqrt(cSquare);
+                // int cSecond = (int)sqrt(cSquare);
 
-                if(cSecond * cSecond == cSquare && cSecond <= n) {
-                    cout << a << " " << b << " " << cSquare  << " " << cSecond <<  endl;
+                // if(cSecond * cSecond == cSquare && cSecond <= n) {
+                //     cout << a << " " << b << " " << cSquare  << " " <<
+                // cSecond <<  endl;
+                //     count++;
+                // }
+
+                if(perfectSquare(cSquare) && (sqrt(cSquare)) <= n){
+                    cout << a << " " << b << " "  << cSquare <<  endl;
                     count++;
                 }
 
@@ -22,5 +56,11 @@ public:
 
         return count;
 
+        // for (int i = 1; i < n * n; i++) {
+        //     if (perfectSquare(i)) {
+        //         cout << i << endl;
+        //     }
+        // }
+        // return 0;
     }
 };
