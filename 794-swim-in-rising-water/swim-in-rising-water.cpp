bf0fb87d42ed99a8d27f7151290faaa987493d9d
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-01-28 14:27:33 +0530

    Time: 3 ms (89.45%) | Memory: 13.1 MB (65.08%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        int n = grid.size();
-        int left = grid[0][0];
-        int right = n * n - 1;
+        int low = 0;
+        int high = rows * rows - 1;
+        int ans = high;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canSwim(grid, mid)) {
-                right = mid;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+
+            if(canSwim(grid, mid)) {
+                ans = mid;
+                high = mid;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
 
-        return left;
+        return ans;
+
     }
 
     bool canSwim(vector<vector<int>>& grid, int t) {
 
-        int n = grid.size();
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid.size(), false));
         return dfs(grid, visited, 0, 0, t);
-        
+
     }
 
-    bool dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int i,
-             int j, int t) {
+    bool dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int r, int c, int t) {
 
-        int n = grid.size();
-
-        if (i < 0 || i >= n || j < 0 || j >= n || visited[i][j] ||
-            grid[i][j] > t) {
+        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || grid[r][c] > t || visited[r][c]) {
             return false;
         }
 
-        if (i == n - 1 && j == n - 1) {
+        if(r == grid.size() - 1 && c == grid.size() - 1) {
             return true;
         }
 
-        visited[i][j] = true;
+        visited[r][c] = true;
 
-        return (dfs(grid, visited, i - 1, j, t) ||
-                dfs(grid, visited, i + 1, j, t) ||
-                dfs(grid, visited, i, j - 1, t) ||
-                dfs(grid, visited, i, j + 1, t));
+        return dfs(grid, visited, r + 1, c, t) || dfs(grid, visited, r - 1, c, t) || dfs(grid, visited, r, c + 1, t) || dfs(grid, visited, r, c - 1, t);
+
     }
 };
