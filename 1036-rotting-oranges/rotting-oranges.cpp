5ef73f0f5d0a4a3249a0b3340ddee5ac7bd2840a
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-01-29 09:51:30 +0530

    Time: 3 ms (28.88%) | Memory: 18 MB (6.06%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,54 +1,58 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-
-        if (grid.size() == 0) {
-            return -1;
-        }
-
-        int freshOranges = 0;
+        
+        int time = -1;
+        int rows = grid.size();
+        int cols = grid[0].size();
         queue<vector<int>> rottenQueue;
+        int fresh = 0;
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-
-                if (grid[i][j] == 1) {
-                    freshOranges++;
-                } else if (grid[i][j] == 2) {
-                    rottenQueue.push({i, j});
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == 1) {
+                    fresh++;
+                } else if(grid[r][c] == 2) {
+                    rottenQueue.push({r, c});
                 }
             }
         }
 
-        if (freshOranges == 0)
+        if(fresh == 0) {
             return 0;
+        }
 
-        int minutes = 0;
-        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!rottenQueue.empty()) {
+        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
+        while(!rottenQueue.empty()) {
             int size = rottenQueue.size();
-            for (int i = 0; i < size; i++) {
 
-                vector<int> rotten = rottenQueue.front();
+            for(int i = 0; i < size; i++) {
+
+                vector<int> front = rottenQueue.front();
                 rottenQueue.pop();
 
-                for (auto& dir : directions) {
+                for(auto& dir : directions) {
+                    int x = front[0] + dir[0];
+                    int y = front[1] + dir[1];
 
-                    int x = rotten[0] + dir[0];
-                    int y = rotten[1] + dir[1];
-
-                    if (x >= 0 && x < grid.size() && y >= 0 &&
-                        y < grid[0].size() && grid[x][y] == 1) {
+                    if(x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 1) {
+                        fresh--;
                         grid[x][y] = 2;
-                        freshOranges--;
                         rottenQueue.push({x, y});
                     }
                 }
+
             }
-            minutes++;
+            time++;
+
         }
 
-        return freshOranges ? -1 : minutes - 1;
+        if(fresh) {
+            return -1;
+        }
+
+        return time;
+
     }
 };
