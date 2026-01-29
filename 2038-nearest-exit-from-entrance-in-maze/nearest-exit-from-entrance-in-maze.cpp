d142aff95c36e0a511edeffb988a75893d6fafb8
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-01-29 11:19:29 +0530

    Time: 4 ms (79.71%) | Memory: 35.4 MB (71.60%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+
+        int rows = maze.size();
+        int cols = maze[0].size();
+
+        queue<pair<int,int>> q;
+        q.push({entrance[0], entrance[1]});
+        maze[entrance[0]][entrance[1]] = '+';
+
+        int steps = 0;
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        while(!q.empty()) {
+            int size = q.size();
+            steps++;
+
+            while(size--) {
+                auto [r, c] = q.front();
+                q.pop();
+
+                for(auto& d : dirs) {
+                    int x = r + d[0];
+                    int y = c + d[1];
+
+                    if(x < 0 || y < 0 || x >= rows || y >= cols || maze[x][y] != '.')
+                        continue;
+
+                    if(x == 0 || y == 0 || x == rows-1 || y == cols-1)
+                        return steps;
+
+                    maze[x][y] = '+';
+                    q.push({x, y});
+                }
+            }
+        }
+        return -1;
+    }
+};
