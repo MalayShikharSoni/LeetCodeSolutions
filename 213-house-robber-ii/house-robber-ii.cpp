ea94de811cc018da856f8801322e355de89ffa92
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-02-01 20:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.45%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.size() == 1) {
+            return nums[0];
+        }
+
+        int rob1 = 0;
+        int rob2 = 0;
+        int max1 = 0;
+        int max2 = 0;
+
+        for(int i = 0; i < nums.size() - 1; i++) {
+
+            max1 = max(rob2, rob1 + nums[i]);
+            rob1 = rob2;
+            rob2 = max1;
+
+        }
+
+        rob1 = 0;
+        rob2 = 0;
+
+        for(int i = 1; i < nums.size(); i++) {
+            
+            max2 = max(rob2, rob1 + nums[i]);
+            rob1 = rob2;
+            rob2 = max2;
+
+        }
+
+        return max(max1, max2);
+
+    }
+};
