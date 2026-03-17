1c5d39131429ff430df3fe6016c47646e6f1c67c
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-03-17 11:39:06 +0530

    Time: 3 ms (24.99%) | Memory: 10.7 MB (58.20%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> ans(n + 1);
+        for(int i = 0; i <= n; i++) {
+
+            int x = i;
+            int count = 0;
+            while(x > 0) {
+                if(x & 1) {
+                    count++;
+                }
+                x = x >> 1;
+            }
+
+            ans[i] = count;
+
+        }
+
+        return ans;
+
+    }
+};
