f093b5d9a8dc9233af80695df1ff1e97edf3e135
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-03-22 22:26:22 +0530

    Time: 7 ms (91.18%) | Memory: 35 MB (87.22%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,55 +1,57 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int n=weights.size();
-        int maxx=weights[0];
-        int sum=0;
-
-        for(int i=0;i<n;i++){
-            sum+=weights[i];
-            if(weights[i]>maxx){
-                maxx=weights[i];
-            }
+        
+        int maxx = 0;
+        int sum = 0;
+        for(int i = 0; i < weights.size(); i++) {
+            maxx = max(maxx, weights[i]);
+            sum += weights[i];
         }
 
-        int low=maxx;
-        int high=sum;
-        int minn=sum;
+        int low = maxx;
+        int high = sum;
+        int minn = sum;
 
-        while(low<=high){
-            int mid=(low+high)/2;
+        while(low <= high) {
 
-            if(check(mid,days,weights)){
-                minn=mid;
-                high=mid-1;
+            int mid = low + (high - low) / 2;
+
+            if(check(mid, weights, days)) {
+                minn = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else{
-                low=mid+1;
-            }
+
         }
+
         return minn;
+
     }
 
-    int check(int capacity, int days, vector<int>weights){
-        int n=weights.size();
-        int c=capacity;
-        int daycount=1;
+    bool check(int capacity, vector<int>& weights, int days) {
 
-        for(int i=0;i<n;i++){
-            if(c>=weights[i]){
-                c-=weights[i];
-            }
-            else{
-                daycount++;
-                c=capacity-weights[i];
+        int c = capacity;
+        int dayCount = 1;
 
+        for(int i = 0; i < weights.size(); i++) {
+
+            if(c >= weights[i]) {
+                c -= weights[i];
+            } else {
+                dayCount++;
+                c = capacity - weights[i];
             }
+
         }
 
-        if(daycount<=days){
-            return 1;
+        if(dayCount <= days) {
+            return true;
         }
-        return 0;
+
+        return false;
+
     }
-    
+
 };
