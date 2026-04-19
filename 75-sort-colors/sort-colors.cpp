8895938f047d1c870a6b199621da53ac4aa84e44
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-04-19 22:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.06%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> freq = {{0, 0}, {1, 0}, {2, 0}};
-
-        for (int i = 0; i < nums.size(); i++) {
+        
+        int n = nums.size();
+        vector<int> freq(3, 0);
+        for(int i = 0; i < nums.size(); i++) {
             freq[nums[i]]++;
         }
 
-        int i = 0;
-
-        for (int j = 0; j < 3; j++) {
-            int f = freq[j];
-
-            for (int k = 0; k < f; k++) {
-                nums[i] = j;
-                i++;
-            }
+        for(int i = 1; i < freq.size(); i++) {
+            freq[i] += freq[i - 1];
         }
+
+        vector<int> ans(n);
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            ans[freq[nums[i]] - 1] = nums[i];
+            freq[nums[i]]--;
+
+        }
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = ans[i];
+        }
+
     }
 };
