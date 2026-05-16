d1dda05f6541bb7183698682bbc14d990501082d
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-05-16 14:17:46 +0530

    Time: 211 ms (45.59%) | Memory: 103 MB (41.18%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -8,8 +8,10 @@ public:
         vis.insert(s);
 
         while (!q.empty()) {
-            string cur = q.front(); q.pop();
-            if (cur < smallest) smallest = cur;
+            string cur = q.front();
+            q.pop();
+            if (cur < smallest)
+                smallest = cur;
 
             string added = cur;
             for (int i = 1; i < added.size(); i += 2)
@@ -19,7 +21,8 @@ public:
                 q.push(added);
             }
 
-            string rotated = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
+            string rotated =
+                cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
             if (!vis.count(rotated)) {
                 vis.insert(rotated);
                 q.push(rotated);
