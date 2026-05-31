5c7230f611a07d23556971f1f4e6c426db4f8bb9
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-05-31 15:21:17 +0530

    Time: 48 ms (59.34%) | Memory: 75.8 MB (26.61%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        
         unordered_map<int, unordered_set<int>> reachable;
 
-        for (auto& prereq : prerequisites) {
-            reachable[prereq[1]].insert(prereq[0]);
+        for(auto& pre : prerequisites) {
+            reachable[pre[1]].insert(pre[0]);
         }
 
-        for (int i = 0; i < numCourses; ++i) {
-            for (int j = 0; j < numCourses; ++j) {
-                if (reachable[j].count(i)) {
+        for(int i = 0; i < numCourses; i++) {
+            for(int j = 0; j < numCourses; j++) {
+
+                if(reachable[j].count(i)) {
                     reachable[j].insert(reachable[i].begin(), reachable[i].end());
                 }
+
             }
         }
 
-        vector<bool> result;
-        for (auto& query : queries) {
-            result.push_back(reachable[query[1]].count(query[0]) > 0);
+        vector<bool> ans;
+
+        for(auto q : queries) {
+
+            if(reachable[q[1]].count(q[0])) {
+                ans.push_back(true);
+            } else {
+                ans.push_back(false);
+            }
+
         }
-        return result;
+
+        return ans;
+
     }
 };
