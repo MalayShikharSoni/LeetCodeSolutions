03019b03df24118361c655c9e49f7408745771ac
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-05-31 15:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (42.35%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int ans = -1;
 
-        auto check = [&](int m) -> bool {
-            int cnt = 1;
-            int sum = 0;
-            for (auto i : nums) {
-                if (sum + i > m) {
-                    cnt++;
-                    sum = i;
-                } else {
-                    sum += i;
+    bool canSplit(vector<int>& nums, int k, int maxSum) {
+
+        int currSum = 0;
+        int subarr = 1;
+
+        for(int num : nums) {
+
+            if(currSum + num > maxSum) {
+                subarr++;
+                currSum = num;
+
+                if(subarr > k) {
+                    return false;
                 }
-            }
-            return cnt <= k;
-        };
 
-        int l = *max_element(nums.begin(), nums.end());
-        int r = accumulate(nums.begin(), nums.end(), 0);
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (check(mid)) {
-                ans = mid;
-                r = mid - 1;
             } else {
-                l = mid + 1;
+                currSum += num;
             }
+
+        }
+
+        return true;
+
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        
+        long long low = nums[0];
+        long long high = 0;
+
+        for(int num : nums) {
+
+            if(num > low) {
+                low = num;
+            }
+            high += num;
+        
+        }
+
+        long long ans = high;
+
+        while(low <= high) {
+
+            long mid = low + (high - low) / 2;
+
+            if(canSplit(nums, k, mid)) {
+
+                ans = mid;
+                high = mid - 1;
+
+            } else {
+
+                low = mid + 1;
+
+            }
+
         }
 
         return ans;
-    
-    }
 
+    }
 };
