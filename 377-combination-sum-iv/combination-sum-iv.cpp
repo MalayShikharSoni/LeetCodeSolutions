364e4015fbdb671cb11a8e225e388c321aaafdae
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-07-08 12:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (91.88%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        
+
         vector<unsigned int> dp(target + 1, 0);
         dp[0] = 1;
 
-        for(int i = 1; i <= target; i++) {
-
+        for(int i = 0; i <= target; i++) {
+            
             for(int num : nums) {
-                if(i - num >= 0) {
+
+                if(num <= i) {
                     dp[i] += dp[i - num];
                 }
             }
 
         }
 
-        return dp[target];
+        return (int)dp[target];
 
     }
 };
