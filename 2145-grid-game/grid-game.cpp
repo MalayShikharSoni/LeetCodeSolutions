608ab0890cd28617cf6be23d3eecf6fc8ebc87f2
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-08-30 16:11:54 +0530

    Time: 2 ms (49.12%) | Memory: 66.7 MB (73.50%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
+        
         long long r1 = 0;
         long long r2 = 0;
 
-        for(int i=0;i<grid[0].size();i++){
+        for(int i = 0; i < grid[0].size(); i++) {
             r1 += grid[0][i];
         }
 
-        long long minAns = LLONG_MAX;
+        long long minPossibleScore = LLONG_MAX;
 
-        for(int i=0;i<grid[0].size();i++){
+        for(int i = 0; i < grid[0].size(); i++) {
+
             r1 -= grid[0][i];
 
-            long long maxScore = max(r1,r2);
-
-            minAns = min(minAns, maxScore);
+            long long maxScore = max(r1, r2);
+            minPossibleScore = min(minPossibleScore, maxScore);
 
             r2 += grid[1][i];
+
         }
 
-        return minAns;
+        return minPossibleScore;
+
     }
 };
