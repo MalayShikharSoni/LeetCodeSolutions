546941276e1b7222dd53ed8473c03b90d61457b3
Malay Shikhar Soni <145752349+MalayShikharSoni@users.noreply.github.com>
2026-09-11 18:44:37 +0530

    Time: 4 ms (31.14%) | Memory: 10.2 MB (58.32%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -6,7 +6,6 @@ public:
         vector<string> board(n, string(n, '.'));
 
         backtrack(0, board, ans);
-
         return ans;
 
     }
@@ -21,37 +20,45 @@ public:
         for(int row = 0; row < board.size(); row++) {
 
             if(isValid(row, col, board)) {
+
                 board[row][col] = 'Q';
                 backtrack(col + 1, board, ans);
                 board[row][col] = '.';
+
             }
 
         }
 
     }
-
+    
     bool isValid(int row, int col, vector<string>& board) {
 
         for(int c = 0; c < col; c++) {
+
             if(board[row][c] == 'Q') {
                 return false;
             }
+
         }
 
         for(int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
+
             if(board[r][c] == 'Q') {
                 return false;
             }
+
         }
 
         for(int r = row, c = col; r < board.size() && c >= 0; r++, c--) {
+
             if(board[r][c] == 'Q') {
                 return false;
             }
+
         }
 
         return true;
 
-    } 
+    }
 
 };
